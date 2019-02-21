bf129fee4dcda42b3b4f2c25280eed1a3e990eab
Yuri Obshansky <yobshans@redhat.com>
2019-02-21 16:21:36 -0500

    Add files via upload

---
+++ scripts/check-api-perf-degradation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$1" != "" ]; then
+  #echo "Directory parameter is $1"
+  TRX_RESULT=$(grep 'FAIL' $1/mrg/compare-api.csv |wc -l)
+  if [[ $TRX_RESULT -gt 0 ]]; then
+    echo "Detected API performance degradation"
+    cat $1/mrg/compare-api.csv | while read line
+    do
+        LINE_RESULT=$(echo $line | cut -d',' -f2)
+        if [[ $LINE_RESULT =  FAIL ]]; then
+            echo $line
+        fi
+    done
+    exit 1
+  else
+    echo "There are no API performance degradation"
+  fi
+else
+  echo "Directory parameter is empty"
+  exit 1
+fi
